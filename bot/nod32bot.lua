cbc238ce3855e62e6a80306174560cebc3b7981a
Samanstar <samanbeeza.net81@gmail.com>
2016-03-15 17:31:53 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {196740258},--Sudo users
+	    sudo_users = {185004428},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
